52709256e45220916d84c154d43b5615cebd7ae6
TsukuiRai <rai0212lab@gmail.com>
2024-12-14 01:40:17 +0900

    5

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <h1>
+        Introduction:
+      </h1>
+      <p>
+        My name is Tsukui Rai.
+        I'm UoA students. 
+      </p>
+      <h2>
+        About me:
+      </h2>
+      <ul>
+        <li>
+          Hobby: Playing table tennis, Driving a car, Watching a movie.
+        </li>
+        <li>
+          Studying language: English, C, Python, React.
+        </li>
+      </ul>
+      <h3>
+        Nice to meet you!!
+      </h3>
     </div>
   );
 }
