27357f3a67c650226987cb32ddc0c3afc48f2f0c
TsukuiRai <rai0212lab@gmail.com>
2025-02-15 21:09:38 +0900

    15, Station5 finished.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,72 @@
 // 参考: https://zenn.dev/b1essk/articles/ssr-with-react-express
 
+// Train3.
+/*
 const express = require('express');
 const app = express();
 const PORT = 9000;
 
+// ルートにアクセスした際のレスポンス
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+// サーバを起動
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+// Train3 ここまで.
+*/
+
+
+
+// Train5.
+//import React from 'react';
+//import ReactDOMServer from 'react-dom/server';
+//import App from '../src/App.tsx';
+
+// const express = require('express');
+//import express from 'express'
+
+//import path from 'path';
+
+// /*
+
+const express = require('express');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const path = require('path');
+
+const App = require('../src/App.tsx').default;
+
+const app = express();
+const PORT = 9003;
+
+app.get('/', (req, res) => {
+  // res.send('Hello world');
+  const AppHTML = ReactDOMServer.renderToString(React.createElement(App, null)); // SSR
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>SSR</title>
+    </head>
+    <body>
+      <div id="root">${AppHTML}</div>
+      <script src="/static/bundle.js"></script>
+    </body>
+    </html>
+  `;
+  res.send(html);
+});
+
 // app.use(express.static("./build"));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+// Train5, ここまで.
+
+// */
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"), // 出力先ディレクトリ
     filename: "server.js", // 出力ファイル名
+    libraryTarget: 'commonjs2',
   },
   target: "node", // Node.js 実行環境用
   externals: [nodeExternals()], // node_modules をバンドルから除外
@@ -18,11 +19,31 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      { // ts-loaderの追加
+        test: /\.tsx?$/, // .ts または .tsx ファイルを対象とする
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      { // CSS Loaderの追加
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            }
+          } 
+        ]
+      }
     ],
   },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx'],
+  },
   devServer: {
-    static: path.join(__dirname, "dist"), // contentBase を static に変更
+    static: path.join(__dirname, "build"), // contentBase を static に変更
     compress: true,
-    port: 9001,
+    port: 9001, // 9000と重複しないよう別のポート番号を指定
   },
 };
